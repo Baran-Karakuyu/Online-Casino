f4fbf970e93ec85ae1b06d4ea0f49e15226b983a
Andrei Oleniuc <Andrei Oleniuc@LAPTOP-KKT92GRT>
2020-04-23 08:37:50 +0200

    BlackJack anfang

--- src/ch/bbbaden/Casino/Games/Blackjack/ViewGameController.java
+++ src/ch/bbbaden/Casino/Games/Blackjack/ViewGameController.java
@@ -7,7 +7,13 @@ package ch.bbbaden.Casino.Games.Blackjack;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -17,6 +23,96 @@ import javafx.fxml.Initializable;
 public class ViewGameController implements Initializable {
 
     private ViewModelBlackJack viewModel;
+    @FXML
+    private Button hitBtn;
+    @FXML
+    private Button holdBtn;
+    @FXML
+    private Button doubleBtn;
+    @FXML
+    private Button putBtn;
+    @FXML
+    private Button backBtn;
+    @FXML
+    private ImageView card1Groupier;
+    @FXML
+    private ImageView card2Groupier;
+    @FXML
+    private ImageView card3Groupier;
+    @FXML
+    private ImageView card4Groupier;
+    @FXML
+    private ImageView card5Groupier;
+    @FXML
+    private ImageView card1Player;
+    @FXML
+    private ImageView card2Player;
+    @FXML
+    private ImageView card3Player;
+    @FXML
+    private ImageView card4Player;
+    @FXML
+    private ImageView card5Player;
+    @FXML
+    private ImageView doubleDownCard;
+    @FXML
+    private Label label;
+    @FXML
+    private Label label11;
+    @FXML
+    private Label creditKonto;
+    @FXML
+    private ImageView chip10;
+    @FXML
+    private ImageView chip50;
+    @FXML
+    private ImageView chip100;
+    @FXML
+    private ImageView chip250;
+    @FXML
+    private ImageView chip1000;
+    @FXML
+    private Label chipsvalue;
+    @FXML
+    private ImageView chips;
+    @FXML
+    private ImageView chips1;
+    @FXML
+    private ImageView chips2;
+    @FXML
+    private ImageView chips3;
+    @FXML
+    private ImageView chips4;
+    @FXML
+    private ImageView chips5;
+    @FXML
+    private ImageView chips6;
+    @FXML
+    private ImageView chips7;
+    @FXML
+    private ImageView chips8;
+    @FXML
+    private ImageView chips9;
+    @FXML
+    private ImageView chips10;
+    @FXML
+    private ImageView chips11;
+    @FXML
+    private ImageView chips12;
+    @FXML
+    private ImageView chips13;
+    @FXML
+    private ImageView chips14;
+    @FXML
+    private ImageView chips15;
+    @FXML
+    private ImageView chips16;
+    @FXML
+    private ImageView chips17;
+    @FXML
+    private ImageView chips18;
+    @FXML
+    private Label endLbl;
 
     public void setViewModel(ViewModelBlackJack viewModel) {
         this.viewModel = viewModel;
@@ -34,5 +130,45 @@ public class ViewGameController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void hitAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void holdAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void doubledownAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void putAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void closeBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void chip10(MouseEvent event) {
+    }
+
+    @FXML
+    private void chip50(MouseEvent event) {
+    }
+
+    @FXML
+    private void chip100(MouseEvent event) {
+    }
+
+    @FXML
+    private void chip250(MouseEvent event) {
+    }
+
+    @FXML
+    private void chip1000(MouseEvent event) {
+    }
     
 }
